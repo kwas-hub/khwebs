db9917999b8a1ca6ee03a171da2745098984150d
kwas-hub <khawar_hafeez@hotmail.de>
2026-05-14 02:16:13 +0200

    cors

--- supabase/functions/pdf-ocr/index.ts
+++ supabase/functions/pdf-ocr/index.ts
@@ -1,8 +1,10 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
+  'Access-Control-Max-Age': '86400',
 };
 
 serve(async (req) => {
